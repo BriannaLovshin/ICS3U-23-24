0d7b94d37ee6499b92d63c9bfc98218e2472c85b
fllctrlz <blovshin@bayviewglen.ca>
2023-09-13 12:54:51 -0400

    homework

---
+++ Unit One - Introduction to Java/src/lesson2/HomeworkOne.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class HomeworkOne {
+    public static void main(String[] args) {
+        System.out.println("Question: What is the largest planet in our solar system? "       );
+        System.out.println("A) Earth");
+        System.out.println("B) Mars");
+        System.out.println("C) Jupiter");
+        System.out.println("D) Saturn");
+        System.out.println("Correct Anwser: C) Jupiter");
+        
+    }
+}
---
+++ Unit One - Introduction to Java/src/lesson2/HomeworkTwo.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class HomeworkTwo {
+    public static void main(String[] args) {
+        System.out.println("Presentation: Exploring Huron-Wendat Heritage"       );
+        System.out.println("Aspects of Huron-Wendat Culture:");
+        System.out.println("- Traditional Practices");
+        System.out.println("- Art and Craftsmanship");
+        System.out.println("- Languege and Storytelling");
+        System.out.println("- Community Traditions");
+        System.out.println("Date: Wednesday, November 15, 2023");
+        System.out.println("Duration: 60 minutes");
+        
+    }
+}
+
